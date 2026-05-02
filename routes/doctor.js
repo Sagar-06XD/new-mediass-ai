3468d917f665fe6f166eb07cc653c65d12d370c7
Sagar swarnkar <sagarswarnkar555@gmail.com>
2026-05-01 23:07:49 -0700

    Delete routes directory

--- routes/doctor.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { fetchDoctors } = require('../controllers/doctorController');
-
-// POST /api/doctor
-router.post('/', fetchDoctors);
-
-module.exports = router;
